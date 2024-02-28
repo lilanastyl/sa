a600b1f68157021ed347f6c8ae40ba0c31777bea
lilanastyl <154732894+lilanastyl@users.noreply.github.com>
2024-02-28 14:45:54 +0200

    Add files via upload

---
+++ bunker/story.py
@@ -0,0 +1,17 @@
+
+#from database import mycursor
+#import random
+
+
+#class story():
+
+    #def __init__(self):
+        #self.story_ = ' '
+
+
+    #def getStory(self):
+        #mycursor.execute('select * from story')
+        #self.story_ = random.choice(mycursor.fetchall())[1]
+        #return self.story_
+
+
